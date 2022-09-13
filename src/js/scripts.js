794d17b82f60e2e3e975a771027afd6e43020340
Artem <mart_22@ukr.net>
2022-09-13 12:15:10 +0300

    Init project

---
+++ src/js/scripts.js
@@ -0,0 +1,41 @@
+'use strict';
+
+import debounce from './modules/debounce';
+import blocksStyles from './modules/blocksStyles';
+import blockPosition from './modules/blockPosition';
+import tableWrapper from './modules/tableWrapper';
+// import toggleContent from './modules/toggleContent';
+import masketInput from './modules/masketInput';
+
+if ('ontouchstart' in document.documentElement) {
+	document.body.classList.add('touchdevice');
+}
+
+function isMobile(agent) {
+	if (agent === void 0) agent = navigator.userAgent;
+
+	return /Android|iPhone|iPad|iPod/i.test(agent);
+}
+
+if (isMobile()) {
+	document.body.classList.add('mobile-user-agent');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	tableWrapper();
+	// toggleContent();
+	masketInput();
+}); // END READY
+
+window.addEventListener('resize', () => {
+	debounce(function () {
+		blockPosition();
+	}, 200);
+});
+
+window.addEventListener('load', () => {
+	debounce(function () {
+		blocksStyles();
+		blockPosition();
+	}, 200);
+});
