ffd9f73bf2eea5c4e28735cad5b626b84a381f2e
Artem <mart_22@ukr.net>
2022-09-13 23:26:02 +0300

    Add intersetion observer for map wrapper in blocksStyles.js

--- src/js/modules/blocksStyles.js
+++ src/js/modules/blocksStyles.js
@@ -1,1 +1,29 @@
-export default function blockStyles() {}
+export default function blockStyles() {
+	if (document.querySelector('#contacts-form__weight')) {
+		document.querySelector('#contacts-form__weight').addEventListener('input', (e) => {
+			e.currentTarget.value = e.currentTarget.value.replace(/\D/g, '').substr(0, 10);
+		});
+	}
+
+	if (document.querySelector('.map-wrapper')) {
+		const observer = new IntersectionObserver(
+			(entries, observer) => {
+				entries.forEach((entry) => {
+					if (entry.isIntersecting && entry.target.dataset.src) {
+						entry.target.insertAdjacentHTML(
+							'beforeend',
+							`
+									<iframe src="${entry.target.dataset.src}" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+								`
+						);
+						observer.unobserve(entry.target);
+					}
+				});
+			},
+			{
+				rootMargin: '50px',
+			}
+		);
+		document.querySelectorAll('.map-wrapper').forEach((item) => observer.observe(item));
+	}
+}
